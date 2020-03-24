ef6e24f8eb98c3ed6fbcce2c54cd3ea454842941
Emiliano Mastragostino <emiliano.mastra@gmail.com>
2020-03-24 16:57:36 +0100

    clean up

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-// css is imported inside index.html
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
